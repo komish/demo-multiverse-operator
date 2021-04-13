62a276096c1d2e909ac38b101088ecbd75c9d975
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-04-13 11:11:46 -0500

    creating api MyCustomKind.demo.example.com/v1beta1

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	demov1alpha1 "github.com/komish/demo-multiverse-operator/api/v1alpha1"
+	demov1beta1 "github.com/komish/demo-multiverse-operator/api/v1beta1"
 	"github.com/komish/demo-multiverse-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
@@ -45,6 +46,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(demov1alpha1.AddToScheme(scheme))
+	utilruntime.Must(demov1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
