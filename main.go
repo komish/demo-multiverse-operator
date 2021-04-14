f3ff9af0ea3efb683c6ff165a56143f869a79f6a
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-04-14 10:31:39 -0500

    operator-sdk create webhook --conversion --group demo --kind MyCustomKind --version v1beta1

--- main.go
+++ main.go
@@ -88,6 +88,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MyCustomKind")
 		os.Exit(1)
 	}
+	if err = (&demov1beta1.MyCustomKind{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "MyCustomKind")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
